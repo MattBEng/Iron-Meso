d4f660daf9919b8ba6c64d11ddf590135d7d835f
MattBEng <75818953+MattBEng@users.noreply.github.com>
2026-08-03 13:37:34 +1000

    fun mode

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    The CACHE name carries a build stamp; changing the deployed sw.js (which
    happens automatically on each build below) invalidates old caches on activate.
 */
-const BUILD = "20260803031144";
+const BUILD = "20260803033412";
 const CACHE = "liftdaddy-" + BUILD;
 const SHELL = ["./", "./index.html"];
 
